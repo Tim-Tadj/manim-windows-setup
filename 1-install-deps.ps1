9edec9c40db3809176e7942b36b6731e35100b61
Tim-Tadj <timtadj@gmail.com>
2022-11-28 12:06:03 +1000

    test3

--- 1-install-deps.ps1
+++ 1-install-deps.ps1
@@ -7,7 +7,7 @@ if (Get-Command ffmpeg -ErrorAction SilentlyContinue) {
 } else {
     Write-Output "ffmpeg is not installed"
 }
-#testchange2
+#testchange3
 
 Write-Output "ffmpeg"
 $downloadUrl = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
